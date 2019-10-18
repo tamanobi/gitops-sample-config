ce80fc0ae3a50db6dd0e689c1756ef8d4bea4df3
tamanobi <tamanobi@gmail.com>
2019-10-18 20:28:13 +0900

    refactor

--- .github/scripts/check_yaml.sh
+++ .github/scripts/check_yaml.sh
@@ -2,8 +2,11 @@
   
 set -eu
 
-find ${1-.} -name "*.yaml" -type f | while read FILE
+KUBEVAL_PATH=${1-./kubeval}
+DIR=${2-.}
+
+find ${DIR} -name "*.yaml" -type f | while read FILE
 do
     # 注意 kubeval はCRDに対応していないためスキップする (https://kubeval.instrumenta.dev/)
-    /kubeval -o=tap --strict --skip-kinds Issuer,Certificate "${FILE}"
+    ${KUBEVAL_PATH} -o=tap --strict --skip-kinds Issuer,Certificate "${FILE}"
 done
