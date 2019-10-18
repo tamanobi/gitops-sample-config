a36338dc10207c628810c8d8f42807275d20b51d
tamanobi <tamanobi@gmail.com>
2019-10-18 20:29:44 +0900

    ignore kustomization.yaml

--- .github/scripts/check_yaml.sh
+++ .github/scripts/check_yaml.sh
@@ -5,7 +5,7 @@ set -eu
 KUBEVAL_PATH=${1-./kubeval}
 DIR=${2-.}
 
-find ${DIR} -name "*.yaml" -type f | while read FILE
+find ${DIR} -name "*.yaml" -type f -not -name "kustomization.yaml" | while read FILE
 do
     # 注意 kubeval はCRDに対応していないためスキップする (https://kubeval.instrumenta.dev/)
     ${KUBEVAL_PATH} -o=tap --strict --skip-kinds Issuer,Certificate "${FILE}"
